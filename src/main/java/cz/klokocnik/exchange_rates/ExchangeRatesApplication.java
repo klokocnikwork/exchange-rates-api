46a66464222bea0fc0998a4b2fec4d9544168e3c
Filip Klokočník <klokocnik.filip@gmail.com>
2025-11-24 18:34:06 +0100

    Init commit

---
+++ src/main/java/cz/klokocnik/exchange_rates/ExchangeRatesApplication.java
@@ -0,0 +1,14 @@
+package cz.klokocnik.exchange_rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class ExchangeRatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRatesApplication.class, args);
+    }
+
+}
